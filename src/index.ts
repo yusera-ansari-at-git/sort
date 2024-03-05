db3200c8d18669b60f1689bf93c5bf5fb1f8bd23
yusera-ansari-at-git <142092189+yusera-ansari-at-git@users.noreply.github.com>
2024-03-05 11:06:29 +0530

    Linked List Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
@@ -7,3 +8,13 @@ sorter.sort();
 const stringSorter = new Sorter(new CharactersCollection('yusera'));
 stringSorter.sort();
 console.log(stringSorter.collections);
+
+const linkedList = new LinkedList();
+const linkedListData = [21, 89, 243, 90, 0, -1];
+for (let i = 0; i < linkedListData.length; i++) {
+  linkedList.add(linkedListData[i]);
+}
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+console.log(linkedListSorter.collections);
